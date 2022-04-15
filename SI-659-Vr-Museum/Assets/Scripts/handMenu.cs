8fe2df8c4f8f5ea41c76a0d18efd82d145ed426e
Zhiyuan Zhang <zhiyuan.zhang2000@outlook.com>
2022-04-14 21:22:41 -0400

    add teleport hotspot

--- SI-659-Vr-Museum/Assets/Scripts/handMenu.cs
+++ SI-659-Vr-Museum/Assets/Scripts/handMenu.cs
@@ -9,7 +9,7 @@ public class handMenu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        mrp.transform.position = new Vector3(-9f, 2.7f, 3f);
+        mrp.transform.position = new Vector3(-9f, 00f, 3f);
         mrp.transform.eulerAngles = new Vector3(0f, 90f, 0f);
     }
 
@@ -21,25 +21,25 @@ public class handMenu : MonoBehaviour
 
     public void moveToHall1()
     {
-        mrp.transform.position = new Vector3(-9f, 2.7f, 3f);
+        mrp.transform.position = new Vector3(-9f, 0f, 3f);
         mrp.transform.eulerAngles = new Vector3(0f, 90f, 0f);
     }
 
     public void moveToHall2()
     {
-        mrp.transform.position = new Vector3(-6.7f, 2.7f, -16.2f);
+        mrp.transform.position = new Vector3(-6.7f, 0f, -16.2f);
         mrp.transform.eulerAngles = new Vector3(0f, 90f, 0f);
     }
 
     public void moveToHall3()
     {
-        mrp.transform.position = new Vector3(27f, 2.7f, -16f);
+        mrp.transform.position = new Vector3(27f, 0f, -16f);
         mrp.transform.eulerAngles = new Vector3(0f, 90f, 0f);
     }
 
     public void moveToHall4()
     {
-        mrp.transform.position = new Vector3(34f, 2.7f, -4f);
+        mrp.transform.position = new Vector3(34f, 0f, -4f);
         mrp.transform.eulerAngles = new Vector3(0f, 0f, 0f);
     }
 }
